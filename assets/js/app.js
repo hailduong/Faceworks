cec05a176b10516eef429415c6b3d68025edb8d6
Ly Duong <ly.duong@navigosgroup.com>
2015-06-12 10:09:49 +0700

    FaceWorks

---
+++ assets/js/app.js
@@ -0,0 +1,83 @@
+// Setup Face Career with dependencies
+var faceCareer = angular.module('faceCareer', [
+    'ngRoute',
+    'faceCareerControllers',
+    'ngTouch',
+    'pascalprecht.translate',
+    'ngCookies'
+]);
+
+// Configure Route
+faceCareer.config(['$routeProvider',
+    function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'sampleFaceCtrl'
+        }).when('/result', {
+            templateUrl: 'partials/result.html',
+            controller: 'resultCtrl'
+        }).otherwise({redirectTo: '/'});
+        $locationProvider.html5Mode(true);
+    }]);
+
+
+// Configure Translation
+var translationsEN = {
+    WhichJobDoILook: 'Which Job Do I Look?',
+    Privacy: 'Privacy',
+    TermsOfUse: 'Terms of Use',
+    PrivacyUrl: 'http://www.vietnamworks.com/privacy-policy',
+    TermsOfUseUrl: 'http://www.vietnamworks.com/terms-of-use',
+    UseFBProfile: 'Use Your Facebook Profile',
+    PS: 'P.S. We do not keep any photo!',
+    Hello: 'Hello,',
+    MyNameIs: 'My name is',
+    Sorry: 'Sorry if we did not get your job right! <br/> You can tell us about your job, and we will keep you updated with best jobs.',
+    EmailMe: 'Email Me Jobs',
+    EmailMeUrl: 'http://www.vietnamworks.com/jobalert'
+};
+
+var translationsVI = {
+    WhichJobDoILook: 'Trông Bạn Hợp Với Nghề Nào?',
+    Privacy: 'Quy Định Bảo Mật',
+    TermsOfUse: 'Thỏa Thuận Sử Dụng',
+    PrivacyUrl: 'http://www.vietnamworks.com/quy-dinh-bao-mat',
+    TermsOfUseUrl: 'http://www.vietnamworks.com/thoa-thuan-su-dung',
+    UseFBProfile: 'Sử Dụng Hồ Sơ Facebook',
+    PS: 'P.S. Chúng tôi không lưu giữ bất cứ hình ảnh nào!',
+    Hello: 'Xin chào,',
+    MyNameIs: 'Tên tôi là',
+    Sorry: 'Rất tiếc nếu chúng tôi đoán không đúng công việc của bạn! <br/> Hãy cho chúng tôi biết bạn làm gì, chúng tôi sẽ giúp bạn cập nhật những công việc tốt nhất.',
+    EmailMe: 'Gửi Việc Cho Tôi',
+    EmailMeUrl: 'http://www.vietnamworks.com/tao-thong-bao-viec-lam'
+};
+
+faceCareer.config(['$translateProvider', function ($transProvider) {
+    $transProvider.translations('en', translationsEN);
+    $transProvider.translations('vi', translationsVI);
+    $transProvider.preferredLanguage('en');
+    $transProvider.fallbackLanguage('en');
+    $transProvider.useCookieStorage();
+}]);
+
+
+faceCareer.controller('MainControl', ['$translate', '$scope', function ($translate, $scope) {
+
+
+    $scope.activatedLanguage = function () {
+        if ($translate.use() === "en") {
+            return 'en';
+        } else {
+            return 'vi';
+        }
+    }();
+
+    $scope.changeLanguage = function (languageKey) {
+        $translate.use(languageKey);
+        if (languageKey === "en") {
+            $scope.activatedLanguage = 'en';
+        } else {
+            $scope.activatedLanguage = 'vi';
+        }
+    }
+}]);
---
+++ assets/js/controllers.js
@@ -0,0 +1,170 @@
+// Init this Controller Module
+var faceCareerControllers = angular.module('faceCareerControllers', []);
+
+// Services
+faceCareerControllers.factory('sharedResultService', function () {
+    return {
+        sharedResult: 11,
+        numberOfItem: 23
+    }
+});
+
+// Controller
+faceCareerControllers.controller('sampleFaceCtrl', function ($scope, $http, $location, sharedResultService, $translate) {
+    'use strict';
+    $http.get('assets/face_sample.json').success(function (data) {
+        $scope.sampleFaces = data;
+        $scope.numberOfSampleFaces = data.length;
+        $scope.activeImage = sharedResultService.sharedResult;
+        $scope.carouselPosition = ( 11 - sharedResultService.sharedResult) * 200 + 100;
+        $scope.carouselWidth = ($scope.numberOfSampleFaces) * 200 + 20 + "px";
+        var carouselWidth = $('.sample-faces').width();
+        $scope.carouselWrapperPosition = (carouselWidth - (($scope.numberOfSampleFaces) * 200 + 20 )) / 2;
+        $(window).resize(function () {
+            carouselWidth = $('.sample-faces').width();
+            $scope.carouselWrapperPosition = (carouselWidth - (($scope.numberOfSampleFaces) * 200 + 20 )) / 2;
+        })
+    });
+
+
+    $scope.setFaceActive = function ($index) {
+        if ($index === $scope.activeImage) {
+            $location.url('/result?id=' + $index);
+        } else {
+            var oldPosition = $scope.activeImage;
+            $scope.activeImage = $index;
+            $scope.carouselPosition = $scope.carouselPosition + (oldPosition - $scope.activeImage ) * 200;
+        }
+    };
+
+    $scope.srollActiveFaceDown = function () {
+        if ($scope.activeImage < ($scope.numberOfSampleFaces - 1)) {
+            $scope.activeImage++;
+            $scope.carouselPosition = $scope.carouselPosition - 200;
+        }
+
+    };
+
+    $scope.srollActiveFaceUp = function () {
+        if ($scope.activeImage > 0) {
+            $scope.activeImage--;
+            $scope.carouselPosition = $scope.carouselPosition + 200;
+        }
+    };
+
+    $(function () {
+        $('body').keyup(function (e) {
+            if ($location.path() === "/") {
+                if (e.keyCode === 13) {
+                    $scope.$apply(function () {
+                        $location.url('/result?id=' + $scope.activeImage);
+                    });
+                }
+                if (e.keyCode === 39) {
+                    $scope.$apply(function () {
+                        $scope.srollActiveFaceDown();
+                    });
+
+                }
+                if (e.keyCode === 37) {
+                    $scope.$apply(function () {
+                        $scope.srollActiveFaceUp();
+                    });
+
+                }
+            } else {
+                if (e.keyCode === 27) {
+                    $scope.$apply(function () {
+                        $location.url('/');
+                    });
+                }
+            }
+        })
+    })
+});
+
+faceCareerControllers.controller('resultCtrl', function ($scope, $http, $location, sharedResultService) {
+    $http.get('assets/face_sample.json').success(function (data) {
+        sharedResultService.sharedResult = parseInt($location.search().id, 10);
+        sharedResultService.numberOfItem = data.length;
+        for (i = 0; i < data.length; i++) {
+            if (i === sharedResultService.sharedResult) {
+                $scope.faceResult = data[i];
+            }
+        }
+
+        $scope.$watch(function () {
+            return sharedResultService.sharedResult;
+        }, function () {
+            $location.url('/result?id=' + sharedResultService.sharedResult);
+        });
+    });
+
+
+    $scope.backHome = function () {
+        $location.url('/')
+    };
+
+    $(function () {
+        $('body').keyup(function (e) {
+            if ($location.path() !== "/") {
+                if (e.keyCode === 27) {
+                    $scope.$apply(function () {
+                        $scope.backHome();
+                    });
+                }
+            }
+        })
+    })
+
+});
+
+faceCareerControllers.directive('ngMouseWheelUp', function () {
+    return function (scope, element, attrs) {
+        element.bind("DOMMouseScroll mousewheel onmousewheel", function (event) {
+
+            // cross-browser wheel delta
+            var event = window.event || event; // old IE support
+            var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
+
+            if (delta > 0) {
+                scope.$apply(function () {
+                    scope.$eval(attrs.ngMouseWheelUp);
+                });
+
+                // for IE
+                event.returnValue = false;
+                // for Chrome and Firefox
+                if (event.preventDefault) {
+                    event.preventDefault();
+                }
+
+            }
+        });
+    };
+});
+
+faceCareerControllers.directive('ngMouseWheelDown', function () {
+    return function (scope, element, attrs) {
+        element.bind("DOMMouseScroll mousewheel onmousewheel", function (event) {
+
+            // cross-browser wheel delta
+            var event = window.event || event; // old IE support
+            var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
+
+            if (delta < 0) {
+                scope.$apply(function () {
+                    scope.$eval(attrs.ngMouseWheelDown);
+                });
+
+                // for IE
+                event.returnValue = false;
+                // for Chrome and Firefox
+                if (event.preventDefault) {
+                    event.preventDefault();
+                }
+
+            }
+        });
+    };
+});
